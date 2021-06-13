3fd424a9fbf5bec63435e2993e68dfa1393a5d7f
RegiSchoeler <73962193+RegiSchoeler@users.noreply.github.com>
2021-06-13 07:36:33 -0400

    Delete Topics directory

--- Topics/String arrays/Guard On The Gate/src/Main.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-    var backFromTheWall = readLine()!!.split(',').map { it }.toTypedArray()
-    val returnedWatchman = readLine()!!.toString()  
-    // do not touch the lines above
-    // write your code here   
-
-}
--- Topics/While loops/Collatz conjecture/src/Main.kt
+++
@@ -1,8 +0,0 @@
-fun main() {
-    var n = readLine()!!.toInt()
-    while (n != 1) {
-        print("$n ")
-        if (n % 2 == 0) n /= 2 else n = n * 3 + 1
-    }
-    print(n)
-}
